42a2aacaae6740eae4a8a2d0896dca1ec0439cb1
Jeff <jedayr9074@hotmail.com>
2019-06-10 19:17:41 -0500

    fourth commit

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 const http = require('http')
 const fs = require('fs');
 const path = require('path');
-
-const server = http.createServer((request, response))
-
-let requestGlobal = ''
-
-function handler(err) {
-    if(err) {
-        console.log('El archivo no existe');
-    }
-     else {
-         fs.readFile(filePath, 'utf8', handleRead)
-     }
-}
-function handleRead (err, data){
-    if(err) {
-       console.log('El archivo no existe')
-    }
-    else {
-        console.log(data)
-    }
-}   
-
+const port = 9000
+let globalResponse = ''
 const filePath = path.resolve(__dirname, 'static/index.html')
-fs.access(filePath, fs.constants.F_OK, handler)
-fs.readFile(filePath, 'utf8', handleRead)
+
+function name(err, data){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data.toString());
+        globalResponse.writeHead(200, {'Content-Type': 'text-plain'})
+        globalResponse.write(data.toString());
+        globalResponse.end()
+        console.log('funcion name')
+    }
+}
+
+function name2(err){
+    if(err){
+        console.log(err)
+    }else{
+        fs.readFile(filePath, name)
+        console.log('funcion name2')
+    }
+}
+
+function responseHandler(req, res){
+    globalResponse = res
+    fs.access(filePath, fs.constants.F_OK, name2)  
+    console.log('function responseHnadler')
+}
+
+const server = http.createServer(responseHandler);
+server.listen(port);
